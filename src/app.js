dbb9552160e9bf43e879e9bc6fb2bf69c5714d0a
PradumnUpadhyay <pradumn.upadhyay1@gmail.com>
2020-06-06 21:05:17 +0530

    Commit for chutiya harshit

--- src/app.js
+++ src/app.js
@@ -5,8 +5,28 @@ const jwt=require('jsonwebtoken')
 const port=process.env.PORT || 3000
 
 // GET auth/checkauth
-app.get('auth/checkauth',(req,res)=>{
+app.get('/auth/checkauth',(req,res)=>{
     res.send('auth')
+})
+
+//POST /billing/contracts/calculate-premium
+app.post('/billing/contracts/calculate-premium',(req,res)=>{
+    
+})
+
+// GET contracts
+app.get('/group/contracts',(req,res)=>{
+
+})
+
+// GET validate
+app.get('/group/validate',(req,res)=>{
+
+})
+
+// GET 
+app.get('/contracts/check-active-coverage/:subscriberIdentifier/:effectiveDt',(req,res)=>{
+
 })
 
 
